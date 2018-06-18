c04492e30d4d3dac34b05103ece3abb4177f6298
nn4343 <gea01441@nifty.com>
2018-06-18 22:03:26 +0900

    変更中

--- m1/UnityProject/Assets/Scene/9000_Test/sc/TouchDstControl.cs
+++ m1/UnityProject/Assets/Scene/9000_Test/sc/TouchDstControl.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public partial class TouchDstControl  {
     public Camera m_touchcam;
+    public Camera m_orhcam;
     public Vector3? m_dst_pos;
+    public GameObject m_dstmark;
     public GameObject m_rkt;
     public float      m_dst_speed = 10;
+
+    public float     m_screen_width;
+    public float     m_screen_height;
+    public float     m_width;
+    public float     m_height;
+
+    public MonoBehaviour m_mono;
 
     public void Start()
     {
@@ -19,19 +28,58 @@ public partial class TouchDstControl  {
     }
     void touch_update()
     {
-        if (Input.touchCount==0) return;
-        var spos = Input.mousePosition;
-        var wpos = m_touchcam.ScreenToWorldPoint(spos);
-        m_dst_pos = VectorUtil.Mod_X(m_rkt.transform.position, wpos.x);
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        {
+            var h = m_orhcam.orthographicSize * 2;
+
+            var spos = Input.mousePosition;
+            var x = (spos.x - m_screen_width * 0.5f)  * (h/m_screen_height);
+                
+            m_dst_pos = VectorUtil.Mod_X(m_rkt.transform.position, x);
+        }
+
     }
     bool touch_done()
     {
         return m_dst_pos!=null;
     }
-    void dst_move()
-    {
 
+    bool m_move_done;
+    void dst_move(float speed = 1)
+    {
+        m_move_done = false;
+        m_dstmark.transform.position = (Vector3)m_dst_pos;
+        //m_rkt.transform.position     = (Vector3)m_dst_pos;
+        m_mono.StartCoroutine(dst_move_co(speed));
     }
+
+    IEnumerator dst_move_co(float speed)
+    {
+        while(true)
+        {
+            var len = (m_rkt.transform.position -  m_dstmark.transform.position).magnitude;
+            if (len < speed)
+            {
+                m_rkt.transform.position = m_dstmark.transform.position;
+                m_move_done = true;
+                break;
+            }
+            else
+            {
+                var v = m_dstmark.transform.position - m_rkt.transform.position;
+                if (v.x > 0)
+                {
+                    m_rkt.transform.position += Vector3.right * speed; 
+                }
+                else
+                {
+                    m_rkt.transform.position -= Vector3.right * speed; 
+                }
+            }
+            yield return null;
+        }
+    }
+
     bool dst_done()
     {
         return true;
