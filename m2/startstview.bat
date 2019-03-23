dbb0c465671139347d926a673400c009e681685e
nn4343 <gea01441@nifty.com>
2019-03-23 20:55:45 +0900

    no message

--- m2/startstview.bat
+++
@@ -1,4 +0,0 @@
-cds /d %~dp0
-start "" UnityProject\Assets\Scene\9000_Test\doc\DebriControl.psgg
-start "" UnityProject\Assets\Scene\9000_Test\doc\MainControl.psgg
-start "" UnityProject\Assets\Scene\9000_Test\doc\TouchDstControl.psgg
